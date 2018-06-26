a18df6731315e4738dc33354698498b1bc54249c
Przemyslaw Juda <przemyslaw.juda@gmail.com>
2018-06-26 10:49:25 +0200

    Added first test case

---
+++ tests/test_connectivity.py
@@ -0,0 +1,33 @@
+"""
+Test module for connectivity. Execute with pytest.
+"""
+
+import numpy as np
+import mpstool
+
+import pytest
+
+@pytest.fixture
+def cube():
+    return np.array([[[0, 0, 0],
+                      [0, 0, 0],
+                      [1, 0, 0]],
+                     [[0, 1, 1],
+                      [0, 1, 1],
+                      [0, 0, 0]],
+                     [[0, 1, 1],
+                      [0, 1, 1],
+                      [0, 0, 0]]])
+
+
+def test_connected_component(cube):
+    connectivity_array = np.array([[[0, 0, 0],
+                                     [0, 0, 0],
+                                     [1, 0, 0]],
+                                    [[0, 2, 2],
+                                     [0, 2, 2],
+                                     [0, 0, 0]],
+                                    [[0, 2, 2],
+                                     [0, 2, 2],
+                                     [0, 0, 0]]])
+    assert np.alltrue(mpstool.connectivity.get_components(cube) == connectivity_array)
