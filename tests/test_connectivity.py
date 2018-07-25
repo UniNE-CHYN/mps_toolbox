04df4f91f02fbb0f288d9d310c12599042b8f4ca
Przemyslaw Juda <przemyslaw.juda@gmail.com>
2018-07-25 14:17:59 +0200

    Enforce PEP8 on test_connectivity.py

--- tests/test_connectivity.py
+++ tests/test_connectivity.py
@@ -7,15 +7,18 @@ import mpstool
 
 import pytest
 
+
 @pytest.fixture
 def array():
-    return  np.array([[1, 1, 1],
-                      [0, 0, 0],
-                      [1, 0, 1]])
+    return np.array([[1, 1, 1],
+                     [0, 0, 0],
+                     [1, 0, 1]])
+
 
 @pytest.fixture
 def extruded_array(array):
     return np.array([array, array])
+
 
 @pytest.fixture
 def cube():
@@ -29,13 +32,16 @@ def cube():
                       [0, 1, 1],
                       [0, 0, 0]]])
 
+
 def test_threshold():
     image = np.array([[0.3, 3.0], [1.2, 2.4], [2.1, 1.1]])
-    categorical_ref =np.array([[1, 3], [2, 3], [3, 2]])
+    categorical_ref = np.array([[1, 3], [2, 3], [3, 2]])
 
-    thresholds= np.array([1.0, 2.0])
+    thresholds = np.array([1.0, 2.0])
     print(mpstool.connectivity.categorize(image, thresholds))
-    assert np.alltrue(mpstool.connectivity.categorize(image, thresholds) == categorical_ref )
+    assert np.alltrue(mpstool.connectivity.categorize(
+        image, thresholds) == categorical_ref)
+
 
 def test_connected_component(cube):
     connectivity_array_cube = np.array([[[0, 0, 0],
@@ -47,33 +53,37 @@ def test_connected_component(cube):
                                         [[0, 2, 2],
                                          [0, 2, 2],
                                          [0, 0, 0]]])
-    assert np.alltrue(mpstool.connectivity.get_components(cube, background=0) == connectivity_array_cube)
+    assert np.alltrue(mpstool.connectivity.get_components(
+        cube, background=0) == connectivity_array_cube)
+
 
 def test_categories(cube):
-    assert all(mpstool.connectivity.get_categories(cube) == [0,1])
+    assert all(mpstool.connectivity.get_categories(cube) == [0, 1])
+
 
 def test_function_2D(array):
     axis0_connectivity = {0: np.array([1., 0.]), 1: np.array([0., 0.])}
-    axis1_connectivity = {0: np.array([1., 1.]), 1: np.array([1. , 0.5])}
+    axis1_connectivity = {0: np.array([1., 1.]), 1: np.array([1., 0.5])}
 
     axis0_result = mpstool.connectivity.get_function(array, axis=0)
     axis1_result = mpstool.connectivity.get_function(array, axis=1)
 
     for key in axis0_connectivity:
-        assert np.alltrue( axis0_result[key] == axis0_connectivity[key])
+        assert np.alltrue(axis0_result[key] == axis0_connectivity[key])
     for key in axis1_connectivity:
-        assert np.alltrue( axis1_result[key] == axis1_connectivity[key])
+        assert np.alltrue(axis1_result[key] == axis1_connectivity[key])
+
 
 def test_function_3D(extruded_array):
     axis0_connectivity = {0: np.array([1.]), 1: np.array([1.])}
     axis1_connectivity = {0: np.array([1., 0.]), 1: np.array([0., 0.])}
-    axis2_connectivity = {0: np.array([1., 1.]), 1: np.array([1. ,0.5])}
+    axis2_connectivity = {0: np.array([1., 1.]), 1: np.array([1., 0.5])}
 
     axis0_result = mpstool.connectivity.get_function(extruded_array, axis=0)
     axis1_result = mpstool.connectivity.get_function(extruded_array, axis=1)
     axis2_result = mpstool.connectivity.get_function(extruded_array, axis=2)
 
     for key in axis0_connectivity:
-        assert np.alltrue( axis0_result[key] == axis0_connectivity[key])
-        assert np.alltrue( axis1_result[key] == axis1_connectivity[key])
-        assert np.alltrue( axis2_result[key] == axis2_connectivity[key])
+        assert np.alltrue(axis0_result[key] == axis0_connectivity[key])
+        assert np.alltrue(axis1_result[key] == axis1_connectivity[key])
+        assert np.alltrue(axis2_result[key] == axis2_connectivity[key])
