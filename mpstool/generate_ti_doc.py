4897ddbe64b59feda2b4b02531971098bdf175f7
Przemyslaw Juda <przemyslaw.juda@gmail.com>
2018-08-29 14:08:49 +0200

    Fix PEP8 issues in mpstool/generate_ti_doc.py

--- mpstool/generate_ti_doc.py
+++ mpstool/generate_ti_doc.py
@@ -3,6 +3,7 @@
 import mpstool.ti
 import os
 
+
 def build_doc():
     ti_base = mpstool.ti.build_TrainingImageBase()
     with open('ti_list.inc', 'w') as ti_list_file:
@@ -12,13 +13,16 @@ def build_doc():
             ti_list_file.write('   ' + key + '\n')
             with open(filename(key), 'w') as ti_file:
                 ti_file.write(key + '\n')
-                ti_file.write('========================' + '\n' +'\n')
-                ti_file.write('.. image:: ' + os.path.relpath(ti_base[key].info['image']) + '\n' + '\n')
+                ti_file.write('========================' + '\n' + '\n')
+                ti_file.write(
+                    '.. image:: ' + os.path.relpath(ti_base[key].info['image'])
+                    + '\n' + '\n')
                 ti_file.write(ti_base[key].info['type'] + '\n')
 
 
 def filename(name):
     return name + '.rst'
+
 
 if __name__ == '__main__':
     build_doc()
