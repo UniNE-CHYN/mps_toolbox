dfe1a7131d349003d79894beeecf432c83b060ae
Philippe Renard <philippe.renard@unine.ch>
2018-09-24 16:39:55 +0200

    Slight modif individual TIs

--- ti_example.py
+++ ti_example.py
@@ -2,8 +2,13 @@
 
 import mpstool.ti
 
+
+print("read base")
 ti_base = mpstool.ti.build_TrainingImageBase()
 print("The complete base: ", ti_base.as_dict())
 
+
+print(" ")
+print(" selection:")
 subset_ti = ti_base.get_subset('type', 'categorical')
 print("Only categorical images: ", subset_ti.as_dict())
