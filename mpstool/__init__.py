0eb0c1997b8138696903e865dc1e883937af2acf
Przemysław Juda <33281123+pjuda@users.noreply.github.com>
2020-01-24 14:21:50 +0100

    Remove try in __init__
    Help avoid bugs hard to find

--- mpstool/__init__.py
+++ mpstool/__init__.py
@@ -1,6 +1,3 @@
-try:
-    import mpstool.connectivity
-    import mpstool.stats
-    import mpstool.img
-except ImportError:
-    pass
+import mpstool.connectivity
+import mpstool.stats
+import mpstool.img
